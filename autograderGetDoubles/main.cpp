027aa4e7a8321fab0291af866408f983adde4e6c
aolmsted <39239480+aolmsted@users.noreply.github.com>
2020-09-30 08:39:18 -0400

    Fixes

--- autograderGetDoubles/main.cpp
+++ autograderGetDoubles/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void runtest(int testdata) {
     double *data;
     data = getDoubles(testdata);
-   if(data[0] == 0 && data[1] == 1/3)
+   if((int)(round(data[0] * 100)) == 33 && (int)(round(data[1] * 100)) == 67)
         printf("Passed All Tests");
      else
         cout << "Failed test on int: " << testdata << '\n';
@@ -18,7 +18,7 @@ void runtest(int testdata) {
 int main(int argc, char** argv) {
     int arg = atoi(argv[1]);
     if(arg == 1) {
-      runtest(1);
+      runtest(2);
     }
     if(arg == 2) {
       runtest(10);
--- autograderGetDoubles/solution/getdoubles.cpp
+++ autograderGetDoubles/solution/getdoubles.cpp
@@ -5,7 +5,9 @@ double* getDoubles(int numDoubles)
 {
   double* data = new double[numDoubles];
   for(int i = 1; i <= numDoubles; i++)
-    data[i-1] = 1/3;
+  {
+    data[i-1] = i/3.0;
+  }  
   return data;  
 
 }
